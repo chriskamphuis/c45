6abc31837dd9afc7004c756d6bcedf66dfcca8f1
jfbaaij <jochem.baaij@gmail.com>
2016-01-24 18:48:56 +0100

    Small changes (eg the discrete getGain)

--- node.py
+++ node.py
@@ -5,7 +5,7 @@ import math
 class Node:
     """
     This class represents the nodes of the tree. These nodes can either be
-    leafes or have childs.
+    leafes or have childs. Complexity: O(a*N^2log(N)) or O(a*N^3*log(N))
     """
     def __init__(self, X, y, nodeID, mostPresentClassParent="", minsplit=100):
         if(len(X) != len(y)):
@@ -16,12 +16,15 @@ class Node:
         self.toSplit = None  # Attribute childs are based on
         self.splitValue = None  # If < then this: go to left else right
         if len(X) is 0:
-            self.classify = mostPresentClassParent
+            self.classify = mostPresentClassParent 
+            print "Node {0} (class: {1}).".format(nodeID,self.classify) 
         elif len(X) < minsplit:
             self.classify = self.mostPresent(y)
+            print "Node {0} (class: {1}).".format(nodeID,self.classify) 
         elif len(set(y)) is 1:
             self.mostPresentClass = self.mostPresent(y)
             self.classify = y[0]
+            print "Node {0} (class: {1}).".format(nodeID,self.classify) 
         else:
             self.mostPresentClass = self.mostPresent(y)
             info = self.information(y)
@@ -31,7 +34,7 @@ class Node:
             Xtemp = X
             ytemp = y
             i = 0
-            for attribute in X.T:
+            for attribute in X.T: #O(N^3*a) or O(N^2*a)
                 gain, index, indices = self.getGain(attribute, y, info)
                 if gain > bestGain:
                     bestGain = gain
@@ -44,7 +47,7 @@ class Node:
             X = Xtemp
             y = ytemp
             if bestGain >0:
-                print "Node {3} split on attribute {0}, at a value of {1}, with a gain of {2} .".format(bestAttribute,X[bestIndex][bestAttribute],bestGain, nodeID)            
+                print "Node {3} (class: {4}) split on attribute {0}, at a value of {1}, with a gain of {2} .".format(bestAttribute,X[bestIndex][bestAttribute],bestGain, nodeID, self.mostPresentClass)            
                 self.toSplit = bestAttribute
             elif bestGain is 0:
                 print "No split had to be made"
@@ -64,11 +67,10 @@ class Node:
             self.classify = self.mostPresentClass
 
     """
-    This function looks which class is most present in the trainingsdata, if
-    there are less than minsplit attributes but there are more then 0
-    attributes this will become the leaf label. Its value is also saved
-    otherwise because it should be given to the child node if this one has no
-    values.
+    This function looks which class is most present in the training data. If
+    there are less than minsplit but more than 0 attributes, this will become 
+    the leaf label. Its value is also saved otherwise because it should be 
+    given to the child node if this one has no values.
     """
     def mostPresent(self, y):
         counter = {}
@@ -88,17 +90,16 @@ class Node:
     """
     Determine the gain when splitting on attribute X with target y. Both should
     be the same size, X should contain the sample values of that attribute
-    while y should represent the class of that sample.
+    while y should represent the class of that sample. Complexity: O(N^3) or O(N^2)
     """
     def getGain(self, X, y, information):
         if isinstance(X[0], str):  # Return gain value and empty sting
             coupled = np.array([list(X), list(y)]).T
             indices = np.argsort(X)
             coupled = coupled[indices]
-            possibleValues = list(set(X))
             gain = information
-            for i in range(len(possibleValues)):
-                gain -= self.information([y for [x, y] in coupled if x is i])
+            for i in range(len(X.T)): 
+                gain -= self.information([y for [x, y] in coupled if x == i]) #O(len(possVal)*len(coupled)*len(y)) => O(a*N^2)
             return gain, None, None
         else:  # if numeric return gain and split point (str)
             coupled = np.array([list(X), list(y)]).T
@@ -107,7 +108,7 @@ class Node:
             targets = [b for [a, b] in coupled]
             currentBestIndex = None
             currentBestGain = 0
-            for i in range(len(coupled)):
+            for i in range(len(coupled)): #O(len(coupled)*len(y)) => O(N^2)
                 gain = information - self.information(targets[:i])
                 gain = gain - self.information(targets[i:])
                 if gain > currentBestGain:
@@ -115,15 +116,18 @@ class Node:
                     currentBestIndex = i
             return currentBestGain, currentBestIndex, indices
 
+    """
+    Calculate the information of a given set of classes. Complexity: O(N)
+    """
     def information(self, y):
         n = float(len(y))
         counter = {}
-        for e in y:
+        for e in y: #O(len(y))
             try:
                 counter[e] += 1
             except KeyError:
                 counter[e] = 1
-        counts = [float(value) for value in counter.values()]
+        counts = [float(value) for value in counter.values()] #O(len(y))
         info = 0.0
         for e in counts:
             info += float((-e/n)) * math.log(e/n, 2)
